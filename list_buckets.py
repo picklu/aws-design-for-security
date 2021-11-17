cab815c7e44fbe1a022a0e1c569bb4d4f74d20bd
Subrata Sarker <subrata_sarker@yahoo.com>
2021-11-17 17:38:23 +0600

    Use a function to list the objects in all the buckets

--- list_buckets.py
+++ list_buckets.py
@@ -3,7 +3,24 @@ import boto3
 
 s3 = boto3.resource('s3')
 
-for bucket in s3.buckets.all():
-    print("==>", bucket.name)
-    for object in bucket.objects.all():
-        print("   *", object.key)
+def list_bucket_objects():
+    """ list all the objects in all the buckets 
+    """
+    items = {}
+
+    for bucket in s3.buckets.all():
+        items[bucket.name] = []
+        for object in bucket.objects.all():
+            items[bucket.name].append(object.key)
+    
+    return items
+
+
+if __name__ == "__main__":
+    items = list_bucket_objects()
+    for bucket_name in items.keys():
+        print("==>", bucket_name)
+        for object_key in items[bucket_name]:
+            print("   *", object_key)
+        
+    print("done!")
